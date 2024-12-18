7d7f85b9d45a4cf1f3355a3a0fbfbb2525573539
5Amogh <amoghavarsh@navadhiti.com>
2024-12-18 13:09:00 +0530

    server script issue resolved

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 1819;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use('/mohini', express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
+app.get('/mohini/*', (req, res) => {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });

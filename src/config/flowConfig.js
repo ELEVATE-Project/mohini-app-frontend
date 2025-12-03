739eed7e5bfe6da4d0d84dac30a5f8ce681f4cae
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2025-12-03 13:31:02 +0530

    Refactor VoiceTextInput component and update flow configuration
    - Improved code consistency by adding semicolons in `VoiceTextInput.jsx`.
    - Enhanced textarea height adjustment logic for better user experience.
    - Updated flow configuration in `flowConfig.js` to use websocket routes for various session flows.
    - Cleaned up imports and organized code structure for improved readability.

--- src/config/flowConfig.js
+++ src/config/flowConfig.js
@@ -3,7 +3,7 @@ import ROUTES from "../url";
 import ptmQuestions from "../services/const/questions/ptmQuestions";
 import ylcQuestions, { ylcStoryTextAudio } from "../services/const/questions/ylcQuestions";
 import env from "../utils/env";
-import { bot_routes } from "../configure";
+import { bot_routes, bot_websocket } from "../configure";
 
 const base_path = env.AUDIO_PATH();
 
@@ -83,14 +83,14 @@ export const FLOW_TO_ROUTE_MAP = {
 };
 
 export const FLOW_TO_WEBSOCKET_MAP = {
-  [sessionFlowName.GuestDiscussion]: bot_routes.shikshalokam_chaupal,
-  [sessionFlowName.LoginDiscussion]: bot_routes.shikshalokam_chaupal,
-  [sessionFlowName.ListeningActivity]: bot_routes.listening_activity,
+  [sessionFlowName.GuestDiscussion]: bot_websocket.shikshalokam_chaupal,
+  [sessionFlowName.LoginDiscussion]: bot_websocket.shikshalokam_chaupal,
+  [sessionFlowName.ListeningActivity]: bot_websocket.listening_activity,
   [sessionFlowName.GuestMiStory]: {
-    normal: bot_routes.normal,
-    oneshot: bot_routes.oneshot,
+    normal: bot_websocket.normal,
+    oneshot: bot_websocket.oneshot,
   },
-  [sessionFlowName.SchoolSurvey]: bot_routes.shikshalokam_chaupal,
+  [sessionFlowName.SchoolSurvey]: bot_websocket.shikshalokam_chaupal,
 };
 
 export const getRouteFromSession = (sessionName, selectedType = undefined) => {
@@ -99,7 +99,6 @@ export const getRouteFromSession = (sessionName, selectedType = undefined) => {
   if (selectedType && FLOW_TO_ROUTE_MAP[sessionName][selectedType]) return FLOW_TO_ROUTE_MAP[sessionName][selectedType];
   return bot_routes.reflection;
 };
-
 /**
  * Extracts variable placeholders from a string
  * @param {string} text - The text containing variable placeholders in the format {variableName}

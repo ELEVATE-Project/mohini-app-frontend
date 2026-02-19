08261d07bdd543e91beee00787687c5a66c24dec
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2026-02-19 16:56:07 +0530

    feat: playwright test scripts for mip flow

---
+++ tests/constant/sg-commons-chat.ts
@@ -0,0 +1,29 @@
+/**
+ * MIP (Micro Improvement Plan) flow chat messages
+ * Used in the InitialSwitch step to select MIP flow
+ */
+export const MIP_INITIAL_SWITCH_MESSAGE = "MIP"
+
+/**
+ * Define Challenge step messages
+ * First message describes the challenge area, second elaborates on the problem
+ */
+export const MIP_DEFINE_CHALLENGE_MESSAGES = [
+  "teaching issue in schools",
+  "teachers are not teaching math well",
+  "students are most affected by this",
+  "no",
+  "the local government",
+  "no"
+]
+
+/**
+ * Weeks selection step message
+ * Number of weeks for the MIP (between 1 and 6)
+ */
+export const MIP_WEEKS_SELECTION_MESSAGE = "4"
+
+/**
+ * Title for the MIP project
+ */
+export const MIP_TITLE = "Improving Teaching Quality"

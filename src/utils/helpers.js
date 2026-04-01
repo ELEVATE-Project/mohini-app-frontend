5b8e0584b022e187585bce4408d21f136f906b41
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2026-04-01 09:49:51 +0530

    Fix WebSocket URL mapping for Study Teacher Interview to use the correct bot_websocket reference.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -71,7 +71,7 @@ export function buildWebSocketUrl({ searchParams, storageFlow, selectedType, wss
     [sessionFlowName.LoginDiscussion]: bot_websocket.shikshalokam_chaupal,
     [sessionFlowName.ListeningActivity]: bot_websocket.listening_activity,
     [sessionFlowName.ParentPerceptionSurvey]: bot_websocket.parent_perception_survey,
-    [sessionFlowName.StudyTeacherInterview]: bot_websocket.parent_perception_survey,
+    [sessionFlowName.StudyTeacherInterview]: bot_websocket.study_teacher_interview,
   }
 
   // Type-based flow to websocket mapping

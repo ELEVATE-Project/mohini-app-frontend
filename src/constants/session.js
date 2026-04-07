9d8b134d9fedfb151e9f0748836300c0403cbd0f
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2026-04-07 12:29:05 +0530

    Add new session flows and update dynamic voice chat logic
    - Introduced 'StudyTeacherInterview' and 'BiharTeachersBot' session flows in session constants.
    - Updated dynamic voice chat logic to include 'BiharTeachersBot' in conditions for showing confirm and new session buttons.

--- src/constants/session.js
+++ src/constants/session.js
@@ -24,4 +24,6 @@ export const sessionFlowName = {
   FreeFlow: "free_flow",
   ShikshaSamvad: "shiksha-samvad",
   DelhiShikshaSamvad: "delhi-shiksha-samvad",
+  StudyTeacherInterview: "study_teacher_interview",
+  BiharTeachersBot: "bihar-teachers-bot",
 }

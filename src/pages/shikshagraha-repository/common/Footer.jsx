f389d32652f5fca533ba7a2e89696636d50c8a8e
GSuma21 <21gsuma@gamil.com>
2026-05-04 11:19:03 +0530

    resolved comment

--- src/pages/shikshagraha-repository/common/Footer.jsx
+++ src/pages/shikshagraha-repository/common/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Footer.css";
 import dandelion from "../../../assets/dandelion-footer.svg";
 import dandelion1 from "../../../assets/dandelion-footer-1.svg";
 import dandelion2 from "../../../assets/dandelion-footer-2.svg";
 import dandelion3 from "../../../assets/dandelion-footer-3.svg";
 
 const BASE_URL = "https://shikshagraha.org";
-const JOIN_URL =
+const JOIN_MOVEMENT_FORM_LINK =
   "https://docs.google.com/forms/d/e/1FAIpQLSfSX2bzdJzPBOlstfGg7vWqPFaS5weLnPpwIieR1DBdRgepPg/viewform";
 
 const movementLinks = [
@@ -47,276 +48,9 @@ const socialLinks = [
   },
 ];
 
-const CSS = `
-.sg-footer {
-  position: relative;
-  overflow: hidden;
-}
-
-/* bottom wave image */
-.sg-footer__bg1 {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-}
-
-/* small floating image */
-.sg-footer__bg2 {
-  
-   position: absolute;
-  top: 180px;
-  right: 53%;
-  width: 50px;
- 
-}
-}
-
-/* mid floating image */
-.sg-footer__bg3 {
-     position: absolute;
-  top: 300px;
-  right: 4%;
-  width: 60px;
-}
-
-/* mid floating image */
-.sg-footer__bg4 {
-  position: absolute;
-  top: 300px;
-  right: 4%;
-  width: 60px;
-}
-.sg-footer {
-  background: #562f91;
-  color: white;
-  font-family: Montserrat, sans-serif;
-
-  background-image: url('../../assets/dandelion-footer.svg');
-  background-repeat: no-repeat;
-  background-position: bottom center;
-  background-size: cover;
-   padding-bottom: 260px;
-}
-
-.sg-footer__main {
-  display:flex;
-  flex-wrap:wrap;
-  padding:60px 10%;
-  gap:100px;
-}
-
-/* DEFAULT = LARGE SCREEN (3 columns) */
-.sg-footer__logo-col,
-.sg-footer__right > div {
-  flex: 1;
-}
-
-/* RIGHT SIDE */
-.sg-footer__right {
-  display:flex;
-  flex:2;
-  gap:40px;
-}
-
-/* LEFT */
-.sg-footer__logo-row {
-  display:flex;
-  align-items:center;
-  gap:10px;
-  margin-bottom:12px;
-}
-
-.sg-footer__logo-icon {
-  width:280px;
-}
-
-.sg-footer__tagline {
-  font-size:16px;
-  line-height:1.7;
-  font-weight:600;
-  width: 400px;
-}
-
-/* SOCIAL */
-.sg-footer__socials {
-  display:flex;
-  gap:12px;
-  margin-top:14px;
-}
-
-.sg-footer__social-link {
-  width:36px;
-  height:36px;
-  border-radius:50%;
-  border:1px solid rgba(255,255,255,.4);
-  display:flex;
-  align-items:center;
-  justify-content:center;
-}
-
-/* NAV */
-.sg-footer__nav-heading {
-  font-size:16px;
-  letter-spacing:2px;
-  margin-bottom:18px;
-  font-weight:700;
-}
-
-.sg-footer__nav-list {
-  list-style:none;
-  padding:0;
-}
-
-.sg-footer__nav-link {
-  display:inline-flex;
-  align-items:center;
-  font-size:15px;
-  color:rgba(255,255,255,.85);
-  text-decoration:none;
-  margin-bottom:12px;
-  font-weight:600;
-}
-
-.sg-footer__nav-link::before {
-  content:"-";
-  color:#ff8a3d;
-  margin-right:0;
-  opacity:0;
-  transform:translateX(-6px);
-  transition:.25s;
-}
-
-.sg-footer__nav-link:hover::before {
-  opacity:1;
-  transform:translateX(0);
-  margin-right:6px;
-}
-
-.sg-footer__nav-link:hover {
-  color:white;
-}
-
-/* BUTTON */
-.sg-footer__join-btn {
-  display:inline-block;
-  margin-top:10px;
-  padding:12px 20px;
-  background:#7f3289;
-  border-radius:25px;
-  text-decoration:none;
-  color:white;
-  width:fit-content;
-  font-weight:600;
-  font-size:20px;
-}
-
-.sg-footer__join-btn:hover {
-  background:#fff;
-  color:#7f3289;
-  transform:translateY(-2px);
-}
-
-/* BOTTOM */
-.sg-footer__bottom {
-  border-top:1px solid rgba(255,255,255,.2);
-  padding:20px 1%;
-  display:flex;
-  justify-content:space-between;
-  font-size:14px;
-  fontweight:600;
-  margin: 0px 13%;
-}
-
-/* ---------- TABLET ---------- */
-@media (max-width: 1024px) {
-
-  .sg-footer__logo-col {
-    flex: 0 0 100%;
-  }
-
-  .sg-footer__right {
-    flex: 0 0 100%;
-    justify-content:space-between;
-  }
-
-  .sg-footer__right > div {
-    flex: 0 0 48%;
-  }
-    .sg-footer__tagline {
-  font-size:16px;
-  line-height:1.7;
-  font-weight:600;
-  width: 300px;
-}
-   .sg-footer {
-   padding-bottom: 110px;
-}
-}
-
-/* ---------- MOBILE ---------- */
-@media (max-width: 600px) {
-
-  .sg-footer__right {
-    flex-direction:column;
-  }
-
-  .sg-footer__right > div {
-    // flex: 0 0 100%;
-  }
-    
-  .sg-footer__bottom {
-  border-top:1px solid rgba(255,255,255,.2);
-  padding:20px 2%;
-  display:flex;
-  flex-direction:column;
-  gap:12px;
-  justify-content:center;
-  font-size:14px;
-  fontweight:600;
-   margin: 0px 4%;
- 
-}
-
-.sg-footer__tagline {
-  font-size:16px;
-  line-height:1.7;
-  font-weight:600;
-  width: auto;
-}
-
-.sg-footer__main{
-gap:40px;
-padding:40px 6%;
-}
-
-  .sg-footer__bg2 {
-    display: none;
-  }
-
-  /* mid floating image */
-.sg-footer__bg3 {
-         position: absolute;
-    top: 613px;
-    right: 35%;
-    width: 50px;
-
-}
-
-  .sg-footer {
-   padding-bottom: 100px;
-}
-}
-
-
-`;
-
 export default function Footer() {
   return (
     <>
-      <style dangerouslySetInnerHTML={{ __html: CSS }} />
-
-
       <footer className="sg-footer">
         <img src={dandelion} className="sg-footer__bg1" alt="" />
         <img src={dandelion1} className="sg-footer__bg2" alt="" />
@@ -388,7 +122,7 @@ export default function Footer() {
                   </li>
                 ))}
                 <li>
-                  <a href={JOIN_URL} target="_blank" className="sg-footer__join-btn">
+                  <a href={JOIN_MOVEMENT_FORM_LINK} target="_blank" className="sg-footer__join-btn">
                     Join the Movement
                   </a>
                 </li>
